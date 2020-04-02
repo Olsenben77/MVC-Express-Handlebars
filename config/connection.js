d6ab8b8fc3d693ad95f6a36ac60f29a5c44bd865
Ben Olsen <olsenben1992@gmail.com>
2020-04-02 15:59:27 -0700

    ok

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,15 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Summer2019!",
-  database: "burgers_db"
-});
+const connection = mysql.createConnection(
+  process.env.JAWSDB_URL || {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Summer2019!",
+    database: "burgers_db"
+  }
+);
 
 // Make connection.
 connection.connect(err => {
